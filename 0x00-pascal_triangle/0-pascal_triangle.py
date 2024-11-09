86ce9ffad4da0d1b77ae884af5abceb5a4f16a10
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-11-09 22:55:10 +0100

    update task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,14 +10,12 @@ def pascal_triangle(n):
     the Pascal's triangle of a given integer.
     '''
     triangle = []
-    if type(n) is not int or n <= 0:
+    if n <= 0:
         return triangle
+
     for i in range(n):
-        line = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                line.append(1)
-            elif i > 0 and j > 0:
-                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(line)
     return triangle
