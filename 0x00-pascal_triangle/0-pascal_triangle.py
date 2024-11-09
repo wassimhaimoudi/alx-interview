ca0f32fca015207569a776219845346af70f7d11
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-11-09 22:56:44 +0100

    update task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,5 +17,5 @@ def pascal_triangle(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(line)
+        triangle.append(row)
     return triangle
