1ee4b079319b1f9422df81a9dbf5f89dd54b28a9
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2025-01-06 13:45:42 +0100

    Update file name

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
-        print("Usage: nqeens N")
+        print("Usage: nqeens N", file=sys.stderr)
         exit(1)
 
     try:
